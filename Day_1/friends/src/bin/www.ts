784c707034d9b964877487463cfab3419ccb6985
Jean-Poul <cph-jl360@cphbusiness.dk>
2021-03-11 12:36:51 +0100

    home assignment added

---
+++ Day_1/friends/src/bin/www.ts
@@ -0,0 +1,10 @@
+import app from '../app'
+
+import d from 'debug'
+
+const debug = d('www');
+
+const PORT = process.env.PORT || 3333;
+console.log(PORT);
+
+app.listen(PORT, () => debug(`Server started, listening on PORT: ${PORT}`))
